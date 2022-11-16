5d7c029d3d0986c27ceb1a45f6f61aa136d876e0
Pwensa <92643532+Pwensa@users.noreply.github.com>
2022-11-16 19:56:20 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 #include <math.h>
-
+void pythagoras_hypotenuse(double AB,double AC){
+    double hypotenuse;
+    hypotenuse = sqrt(pow(AB,2)+pow(AC,2));
+    printf("\nThe length of the hypotenuse BC is %lf cm\n",hypotenuse);
+}
+void pythagoras_side(double BC,double AC,double AB,int mode){
+    
+    switch (mode)
+    {
+    case 1:
+        AB = sqrt(pow(BC,2)-pow(AC,2));
+        printf("\nThe length of AB is %lf cm\n",AB);
+        break;
+    
+    case 2:
+        AC = sqrt(pow(BC,2)-pow(AB,2));
+        printf("\nThe length of AC is %lf cm\n",AC);
+        break;
+    }
+}
+void reciprocal_pythagoras(double BC,double AC,double AB){
+    if(pow(AB,2) + pow(AC,2) == pow(BC,2)){
+        printf("\nThe triange ABC is right-angled");
+    }else{
+        printf("\nThe triangle ABC isn't right-angled");
+    }
+}
 //Least common divisor
 void LCM(double value1,double value2){
     double value1a = value1;
@@ -40,39 +66,107 @@ void factorial(double value1){
 int main(void){
     double value1;
     double value2;
-    char operand;
-
-    scanf("%lf %c %lf", &value1,&operand, &value2);
     
+    int mode;
+    char operand;
+    printf("Welcome,\nSelect mode:\n-1 for theorem of pythagoras\n-2 for basic calculator\n-3 for theorem of thales\n\nMode:");
+    scanf("%d",&mode);
 
-    switch (operand)
+    switch (mode)
     {
-    case '+':
-        printf("%lf\n",value1 + value2);
+    case 1:
+
+        double AB = 0;
+        double AC = 0;
+        double BC = 0;
+        mode = 0;
+        printf("B");
+        for(int i = 0;i<=5;i++){
+            for(int d = 0;d<i;d++){
+                printf("*");
+            }
+            printf("\n");
+        }
+        printf("A    C\n");
+        printf("\n");
+        printf("Do you want to calculate:\n-1 for the theorem of pythagoras\n-2 for the theorem of pythagoras to calculate AC or AB\n-3 for the reciprocal pythagoras\n\nMode:");
+        scanf("%d",&mode);
+        switch (mode)
+        {
+        case 1:
+            printf("\nEnter the value of AB:");
+            scanf("%lf",&AB);
+            printf("Enter the value of AC:");
+            scanf("%lf",&AC);
+            pythagoras_hypotenuse(AB,AC);
+            break;
+        
+        case 2:
+            mode = 0;
+            printf("Do you want to calculate:\n-1 for the length of AB\n-2 for length of AC\n\nMode:");
+            scanf("%d",&mode);
+            switch (mode)
+            {
+            case 1:
+                printf("\nEnter the value of BC:");
+                scanf("%lf",&BC);
+                printf("\nEnter the value of AC:");
+                scanf("%lf",&AC);
+                break;
+            
+            case 2:
+                printf("\nEnter the value of BC:");
+                scanf("%lf",&BC);
+                printf("\nEnter the value of AB:");
+                scanf("%lf",&AB);
+                break;
+            }
+            pythagoras_side(BC,AC,AB,mode);
+            break;
+        
+        case 3:
+            printf("\nEnter the value of BC:");
+            scanf("%lf",&BC);
+            printf("\nEnter the value of AC:");
+            scanf("%lf",&AC);
+            printf("\nEnter the value of AB:");
+            scanf("%lf",&AB);
+            reciprocal_pythagoras(AB,AC,BC);
+        }
+        
         break;
-    case '-':
-        printf("%lf\n",value1 - value2);
-        break;
-    case '*':
-        printf("%lf\n",value1 * value2);
-        break;
-    case '/':
-        printf("%lf\n",value1 / value2);
-        break;
-    case '%':
-        printf("%lf\n",fmod(value1,value2));
-        break;
-    case '^':
-        printf("%lf\n",pow(value1,value2));
-        break;
-    case '!':
-        factorial(value1);
-        break;
-    case 'g':
-        GCD(value1,value2);
-        break;
-    case 'l':
-        LCM(value1,value2);
+    case 2:
+        scanf("%lf %c %lf", &value1,&operand, &value2);
+        switch (operand)
+        {
+        case '+':
+            printf("%lf\n",value1 + value2);
+            break;
+        case '-':
+            printf("%lf\n",value1 - value2);
+            break;
+        case '*':
+            printf("%lf\n",value1 * value2);
+            break;
+        case '/':
+            printf("%lf\n",value1 / value2);
+            break;
+        case '%':
+            printf("%lf\n",fmod(value1,value2));
+            break;
+        case '^':
+            printf("%lf\n",pow(value1,value2));
+            break;
+        case '!':
+            factorial(value1);
+            break;
+        case 'g':
+            GCD(value1,value2);
+            break;
+        case 'l':
+            LCM(value1,value2);
+            break;
+        }
         break;
     }
 }
